e92a9e39ce154030ba5b20f4dda037f06b7e69c5
Rob <robmessenger@iMac.lan>
2018-03-01 14:58:21 +0100

    add function to replace UNIQUE question words with blanks. add tests

--- quiz.py
+++ quiz.py
@@ -1,3 +1,10 @@
+def blankQuestionWords(word, question_words):
+    #only supporting unique question words
+    for q in question_words:
+        if q in word:
+            return "_____"
+    return
+
 def poseQuestion(question):
     question_string = question[0].split()
     return question_string
@@ -5,6 +12,14 @@ def poseQuestion(question):
 
 
 def tests():
+    #blankQuestionWords
+    question_words = ["test", "another_test"]
+    test_words = ["test", "test,", "negative"]
+
+    assert blankQuestionWords(test_words[0],question_words) == "_____"
+    assert blankQuestionWords(test_words[1],question_words) == "_____"
+    assert blankQuestionWords(test_words[2],question_words) == None
+
     question = ["this is a test", ["test"]]
 
     assert poseQuestion(question) == ["this", "is", "a", "test"]
